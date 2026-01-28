33cab8af5b0643cafa6eaee00745bb370118e8fc
Moamiine <medamine.teljaoui@gmail.com>
2026-01-28 14:52:11 +0100

    created relationship between posts and categories (foreignkey categories_id in posts table)

--- blog-laravel/app/Models/Post.php
+++ blog-laravel/app/Models/Post.php
@@ -3,8 +3,68 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    
+        private int $id;
+        private string $title;
+        private string $content;
+        private $tags = [];
+
+
+
+        public function getId(): int
+        {
+                return $this->id;
+        }
+
+        public function setId(int $id): self
+        {
+                $this->id = $id;
+
+                return $this;
+        }
+
+        public function getTitle(): string
+        {
+                return $this->title;
+        }
+
+        public function setTitle(string $title): self
+        {
+                $this->title = $title;
+
+                return $this;
+        }
+
+        public function getContent(): string
+        {
+                return $this->content;
+        }
+
+        public function setContent(string $content): self
+        {
+                $this->content = $content;
+
+                return $this;
+        }
+
+        public function getTags()
+        {
+                return $this->tags;
+        }
+
+        public function setTags($tags): self
+        {
+                $this->tags = $tags;
+
+                return $this;
+        }
+
+        public function categorie(): BelongsTo{
+            return $this->belongsTo(Categorie::class, 'categories_id');
+        }
+        
+        
 }
