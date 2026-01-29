bc1fc89a25096ce15253757085f5f0b5e93203b9
Moamiine <medamine.teljaoui@gmail.com>
2026-01-29 16:01:04 +0100

    created function index on home controller to show posts and categories

--- blog-laravel/app/Http/Controllers/HomeController.php
+++ blog-laravel/app/Http/Controllers/HomeController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categorie;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index(){
+        $posts = Post::with('categorie')->get();
+        $categories = Categorie::all();
+        return view('acceuil', compact('posts', 'categories'));
+    }
 }
